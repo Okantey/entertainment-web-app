ca53cf67c1f9b4c745e33f57e9be3d00673e71b4
nii_okantey <nunoogideon19@gmail.com>
2023-09-24 18:33:22 +0000

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages";
 
 const App: React.FC = () => {
   return (
     <React.Fragment>
-      <div className="container">Hello</div>
+      <Routes>
+        <Route path="" element={<Home />} />
+      </Routes>
     </React.Fragment>
   );
 };
