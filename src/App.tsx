0a653b425efb87fcd96a9bf27626207d44c2b3a6
nii_okantey <nunoogideon19@gmail.com>
2023-09-24 18:13:29 +0000

    updates

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <React.Fragment>
+      <div className="container">Hello</div>
+    </React.Fragment>
+  );
+};
+
+export default App;
